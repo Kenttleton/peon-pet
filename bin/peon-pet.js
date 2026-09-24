02d3132f9df3a787e9eebc89d0db48b96f8fb813
Kent <utterback.kent.a@gmail.com>
2026-09-24 15:00:46 -0400

    fix base command

--- bin/peon-pet.js
+++ bin/peon-pet.js
@@ -2,6 +2,9 @@
 'use strict';
 
 const { Command } = require('commander');
+const { spawn } = require('child_process');
+const path = require('path');
+const fs = require('fs');
 const { registerPacksCommand } = require('../lib/cli/packs');
 const { registerSettingsCommands } = require('../lib/cli/settings');
 const { registerStatusCommand } = require('../lib/cli/status');
@@ -12,7 +15,17 @@ const program = new Command();
 program
   .name('peon-pet')
   .description('CLI for configuring and managing peon-pet')
-  .version(require('../package.json').version);
+  .version(require('../package.json').version)
+  .action(() => {
+    const appDir = path.join(__dirname, '..');
+    const electronBin = path.join(appDir, 'node_modules', '.bin', 'electron');
+    if (!fs.existsSync(electronBin)) {
+      process.stderr.write('[peon-pet] Electron not found. Run: npm install\n');
+      process.exit(1);
+    }
+    const child = spawn(electronBin, [appDir], { stdio: 'inherit' });
+    child.on('exit', (code) => process.exit(code || 0));
+  });
 
 registerPacksCommand(program);
 registerSettingsCommands(program);
